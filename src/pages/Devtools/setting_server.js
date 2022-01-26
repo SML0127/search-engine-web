95330e3aba87825fc2c6f809a2b9fdd38725ee9c
SML <semicpu@gmail.com>
2022-01-26 16:49:27 +0900

    Update setting_server.js

--- src/pages/Devtools/setting_server.js
+++ src/pages/Devtools/setting_server.js
@@ -1,10 +1,7 @@
 const setting_server = {
-  //HOST_SERVER: "141.223.197.35",
-  //DB_SERVER: "http://141.223.197.35:5000",
-  //DRIVER_UTIL_SERVER: "http://141.223.197.35:5001",
-  HOST_SERVER: "141.223.197.42",
-  DB_SERVER: "http://141.223.197.42:5000",
-  DRIVER_UTIL_SERVER: "http://141.223.197.42:5001",
+  HOST_SERVER: "",
+  DB_SERVER: "",
+  DRIVER_UTIL_SERVER: "",
   CRAWLING_QUEUE: 'real_queue'
 }
 export default setting_server;
